67dae47fad6eea52d96f7372fdb2c8d863f0c292
你好2007 <2501482523@qq.com>
2021-12-21 14:38:25 +0800

    使用etcpack代替webpack

---
+++ build/main@v0.2.0-bundle1.js
@@ -0,0 +1,54 @@
+
+/*************************** [bundle] ****************************/
+// Original file:./src/sentence.paper
+/*****************************************************************/
+window.__etcpack__bundleSrc__['24']=function(){
+    var __etcpack__scope_bundle__={};
+    var __etcpack__scope_args__;
+    
+
+    // 导入js
+__etcpack__scope_args__=window.__etcpack__getBundle('26');
+var script =__etcpack__scope_args__.default;
+
+
+    // 导入css
+__etcpack__scope_args__=window.__etcpack__getBundle('27');
+
+
+    script.render=function(createElement){
+
+        return createElement('div',{"quickpaper":"","data-quickpaper-5e158485":""},[createElement('div',{"class":"doc-view","id":"root-view","data-quickpaper-5e158485":""},[createElement('header',{"id":"fixed-top","data-quickpaper-5e158485":""},["认识句子"]),createElement('h1',{"id":"fixed-jzcf","data-quickpaper-5e158485":""},["句子成分"]),createElement('ul',{"data-quickpaper-5e158485":""},[createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["主语"]),"：句子所陈述的对象。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["谓语"]),"：主语发出的动作。一般是有动作意义的动词。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["宾语"]),"：分为动词宾语和介词宾语，属于动作的承受者。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["系动词"]),"：表示状态或状态变化的动词，没有实际的动作意义。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["表语"]),"：紧跟系动词后面的成分。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["定语"]),"：修饰名词或代词的成分。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["状语"]),"：修饰形容词、副词、动词或句子的成分。"]),createElement('li',{"data-quickpaper-5e158485":""},[createElement('span',{"class":"important","data-quickpaper-5e158485":""},["补语"]),"：分为宾语补足语和主语补足语。"])]),createElement('h1',{"id":"fixed-jx","data-quickpaper-5e158485":""},["句型"]),createElement('h3',{"id":"fixed-jx-zxb","data-quickpaper-5e158485":""},["主-系-表"]),createElement('p',{"class":"define","data-quickpaper-5e158485":""},["主语+系动词+主语补足语（表语）"]),createElement('p',{"data-quickpaper-5e158485":""},["系动词无具体作用，仅起连接作用，而表语是用来修饰主语的，说明主语的身份、↵            性质、品性、特征和状态。"]),createElement('p',{"class":"eg","data-quickpaper-5e158485":""},["I am fine.（翻译：我很好。）"]),createElement('h3',{"id":"fixed-jx-zw","data-quickpaper-5e158485":""},["主-谓"]),createElement('p',{"class":"define","data-quickpaper-5e158485":""},["主语+谓语（+状语）"]),createElement('p',{"data-quickpaper-5e158485":""},["谓语是不及物动词，有时会带一个状语，用于修饰动作（也就是谓语）。"]),createElement('p',{"class":"eg","data-quickpaper-5e158485":""},["Children giggle.（翻译：孩子们咯咯地笑。）",createElement('br',{"data-quickpaper-5e158485":""},[]),"He works hard.（翻译：他工作很勤奋。）"]),createElement('h3',{"id":"fixed-jx-zwb","data-quickpaper-5e158485":""},["主-谓-宾"]),createElement('p',{"class":"define","data-quickpaper-5e158485":""},["主语+谓语+宾语"]),createElement('p',{"data-quickpaper-5e158485":""},["谓语是及物动词，宾语是主语动作的承受对象。"]),createElement('p',{"class":"eg","data-quickpaper-5e158485":""},["I love china.（翻译：我爱中国。）"]),createElement('h3',{"id":"fixed-jx-zwjbzb","data-quickpaper-5e158485":""},["主-谓-间宾-直宾"]),createElement('p',{"class":"define","data-quickpaper-5e158485":""},["主语+谓语+间宾+直宾"]),createElement('p',{"data-quickpaper-5e158485":""},["间接宾语是接受者（人），直接宾语是承受者（物）。"]),createElement('p',{"class":"eg","data-quickpaper-5e158485":""},["Mum bought my sister a present.（翻译：妈妈给我妹妹买了一份礼物。）"]),createElement('h3',{"id":"fixed-jx-zwbbb","data-quickpaper-5e158485":""},["主-谓-宾-宾补"]),createElement('p',{"class":"define","data-quickpaper-5e158485":""},["主语+谓语+宾语+宾语补足语"]),createElement('p',{"data-quickpaper-5e158485":""},["必须有“宾语补足语”句意才完整。"]),createElement('p',{"class":"eg","data-quickpaper-5e158485":""},["Reading makes me happy.（翻译：读书使我快乐。）"])])])
+
+    };
+
+    __etcpack__scope_bundle__.default= script;
+
+  
+    return __etcpack__scope_bundle__;
+}
+
+/*************************** [bundle] ****************************/
+// Original file:./src/sentence.paper?QuickPaper&type=script&lang=js&hash=5e158485
+/*****************************************************************/
+window.__etcpack__bundleSrc__['26']=function(){
+    var __etcpack__scope_bundle__={};
+    var __etcpack__scope_args__;
+    __etcpack__scope_bundle__.default= {};
+  
+    return __etcpack__scope_bundle__;
+}
+
+/*************************** [bundle] ****************************/
+// Original file:./src/sentence.paper?QuickPaper&type=style&lang=css&hash=5e158485
+/*****************************************************************/
+window.__etcpack__bundleSrc__['27']=function(){
+    var __etcpack__scope_bundle__={};
+    var __etcpack__scope_args__;
+    var styleElement = document.createElement('style');
+var head = document.head || document.getElementsByTagName('head')[0];
+styleElement.innerHTML = "";
+styleElement.setAttribute('type', 'text/css');head.appendChild(styleElement);
+  
+    return __etcpack__scope_bundle__;
+}
--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,4 @@
-import Knowpedia from 'knowpedia';
+import QuickPaper from 'quick-paper';
 
 // 兼容文件
 import '@hai2007/polyfill/Promise.js';
@@ -9,11 +9,10 @@ import './style/style.css';
 import "./style/doc.css";
 import "./style/menu.css";
 
-
 import App from './App.paper';
 
 // 创建对象
-window.knowpedia = new Knowpedia({
+window.quickPaper = new QuickPaper({
 
     // 挂载点
     el: document.getElementById('root'),
